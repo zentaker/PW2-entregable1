d97968573dd137ade1d8336866044c6deea9f9af
zentaker <apalomino12@icloud.com>
2021-04-25 22:49:05 -0500

    arreglo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,13 +9,89 @@ import { PizzaService } from 'src/app/servicios/pizza.service';
 })
 export class HomeComponent implements OnInit {
   pizzas: any[] = [];
+  filtro: any[] = [];
+  categoria: any[] = [];
+  
 
   constructor(private _pizzaService: PizzaService,
-              private router: Router) { }
+    private router: Router) {
+    
+    
+      
+     
+               }
   
   // cuando la pagina ya esta cragada
   ngOnInit(): void {
     this.pizzas = this._pizzaService.getPizzas();
+    this.filtro = this.pizzas;
+
+    //barrer categorias
+  
+    
+  
+    
   }
+
+  //filtro por categorias
+  catGo(termino:string) {
+    let pizzaArr: any[] = [];
+    termino = termino;
+    console.log(termino)
+    for (let i = 0; i < this.pizzas.length; i++){
+
+      let pizza = this.pizzas[i];
+      let categorias = pizza.categorias;
+      
+      for (let i = 0; i < categorias.length; i++) {
+        //console.log(categorias)
+        if ( categorias.indexOf(termino) >= 0) {
+          pizza.idx = i;
+
+          if (pizza.idx == 0) {
+            pizzaArr.push(pizza);
+            this.filtro = pizzaArr;
+          } 
+        }   
+      }
+      
+    }
+    
+    console.log(pizzaArr)
+
+
+    
+  }
+  
+ 
+    /* buscarClinicas (termino:string) {
+    let clinicaArr:any[] = [];
+    termino = termino.toLowerCase();
+
+    for(let i = 0; i< this.clinicas.length; i ++){
+        let clinica = this.clinicas[i];
+        let nombre = clinica.clinica.toLowerCase();
+        let dir = clinica.Direccion.toLowerCase();
+       
+        
+        if (nombre.indexOf(termino) >= 0) {
+            clinica.idx = i;
+            clinicaArr.push(clinica)
+        } else if (dir.indexOf(termino) >= 0){
+            clinica.idx = i;
+            clinicaArr.push(clinica)
+
+        }
+
+    }
+
+
+    return clinicaArr;
+
+
+
+
+} */
+
 
 }
--- src/app/servicios/pizza.service.ts
+++ src/app/servicios/pizza.service.ts
@@ -10,99 +10,120 @@ export class PizzaService {
         "nombre": "Bismarck",
         "precio": "s/45",
         "descripcion": "Pizza para los niños pequeños que les guste las verduras",
-        "categoria1": "Queso",
-        "categoria2": "Para Niños",
-        "categoria3": "Light",
+        "categorias": [
+          "Queso",
+          "Para Niños",
+          "light"
+      
+    ],
         "img": "assets/pizza1.jpg"
     },
     {
       "nombre": "Fiori Di Zucca",
       "precio": "s/45",
       "descripcion": "Para los amantes de los quesos y picante",
-      "categoria1": "Queso",
-      "categoria2": "Para Niños",
-      "categoria3": "picante",
+      "categorias": [
+        "Queso",
+        "picante",
+  ],
       "img": "assets/pizza2.jpg"
     },
     {
       "nombre": "Valdostana",
       "precio": "s/45",
       "descripcion": "Una pizza perfecta para los amantes de los sabores intensos",
-      "categoria1": "Queso",
-      "categoria2": "Para Niños",
-      "categoria3": "Light",
+      "categorias": [
+        "Queso",
+        "Vegetariano",
+        "Picante",
+  ],
       "img": "assets/pizza3.jpg"
     },
     {
       "nombre": "Pizza Tartufata",
       "precio": "s/50",
       "descripcion": "Extra queso con aceite de trufas, un sabor mediterraneo",
-      "categoria1": "Queso",
-      "categoria2": "Para Niños",
-      "categoria3": "dulce",
+      "categorias": [
+        "Queso",
+        "Light",
+        
+  ],
       "img": "assets/pizza4.jpg"
     },
     {
       "nombre": "Francescana",
       "precio": "s/35",
       "descripcion": "Una pizza tradicional francesa para los amantes de la cultura francesa ",
-      "categoria1": "Queso",
-      "categoria2": "Para Niños",
-      "categoria3": "light",
+      "categorias": [
+        "Queso",
+        "Para Niños",
+        "Picante",
+  ],
       "img": "assets/pizza5.jpg"
     },
     {
       "nombre": "Campagnola",
       "precio": "s/45",
       "descripcion": "Muchas verduras para los amantes de las ensaladas en una pizza ",
-      "categoria1": "Queso",
-      "categoria2": "Para Niños",
-      "categoria3": "dulce",
+      "categorias": [
+        "Queso",
+        "Vegetariano",
+        
+  ],
       "img": "assets/pizza6.jpg"
     },
     {
       "nombre": "Boscaiola",
       "precio": "s/75",
       "descripcion": "Muchas verduras para los amantes de las ensaladas en una pizza ",
-      "categoria1": "Queso",
-      "categoria2": "Picante",
-      "categoria3": "dulce",
+      "categorias": [
+        "Queso",
+        "Vegetariano",
+        "picante",
+  ],
       "img": "assets/pizza7.jpg"
   },
     {
       "nombre": "Mare e Monti",
       "precio": "s/45",
       "descripcion": "Pizza marina con langostinos ",
-      "categoria1": "Queso",
-      "categoria2": "light",
-      "categoria3": "Marino",
+      "categorias": [
+        "Queso",
+        "picante",
+  ],
       "img": "assets/pizza6.jpg"
     },
     {
       "nombre": "Parmigiana",
       "precio": "s/45",
       "descripcion": "Pizza marina con langostinos ",
-      "categoria1": "Queso",
-      "categoria2": "light",
-      "categoria3": "Marino",
+      "categorias": [
+        "Queso",
+        "Para Niños",
+        "Picante",
+  ],
       "img": "assets/pizza8.jpg"
     },
     {
       "nombre": "Ricotta e spinac",
       "precio": "s/45",
       "descripcion": "Pizza marina con langostinos ",
-      "categoria1": "Queso",
-      "categoria2": "light",
-      "categoria3": "Marino",
+      "categorias": [
+        "Queso",
+        "Para Niños",
+        "picante",
+  ],
       "img": "assets/pizza9.jpg"
     },
     {
       "nombre": "Pizza al pesto",
       "precio": "s/45",
       "descripcion": "Pizza marina con langostinos ",
-      "categoria1": "Queso",
-      "categoria2": "light",
-      "categoria3": "Marino",
+      "categorias": [
+        "Queso",
+        "Para Niños",
+        "picante",
+  ],
       "img": "assets/pizza10.jpg"
     },
 
@@ -117,4 +138,17 @@ export class PizzaService {
     return this.pizza;
 
   }
+
+  selectCategoria(categoria: string) {
+    let pizzaArr: any[] = [];
+
+    for (let i = 0; i < this.pizza.length; i++){
+
+      console.log(this.pizza[i]);
+      
+    }
+
+  }
+
+
 }
